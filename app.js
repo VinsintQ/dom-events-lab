684494362ddc61dfd429181c19a6e7cdd24afb78
Mueen <hejiy2002@gmail.com>
2024-06-30 08:55:24 +0300

    final

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
-/*-------------------------------- Constants --------------------------------*/
 const button = document.querySelectorAll(".button");
 const display = document.querySelector(".display");
-/*-------------------------------- Variables --------------------------------*/
 let a;
-/*------------------------ Cached Element References ------------------------*/
-/*----------------------------- Event Listeners -----------------------------*/
 button.forEach((element) => {
   element.addEventListener("click", print); //adding event listenr
 });
-/*-------------------------------- Functions --------------------------------*/
 function print(event) {
   if (
-    (display.textContent == "" &&
-      (event.target.textContent === "*" ||
-        event.target.textContent === "=" ||
-        event.target.textContent === "+" ||
-        event.target.textContent === "/")) ||
-    event.target.textContent === "-"
+    display.textContent == "" &&
+    (event.target.textContent === "*" ||
+      event.target.textContent === "=" ||
+      event.target.textContent === "+" ||
+      event.target.textContent === "/" ||
+      event.target.textContent === "-")
   ) {
   } else {
     if (event.target.textContent !== "C" && event.target.textContent !== "=") {
